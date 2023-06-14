1ce632980580ffec6f66d25f3caf068168387aa3
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2023-06-14 11:04:59 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import FiSettings from 'react-icons/fi';
+// import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1 className='underline text-3xl'>I actually love React, but i hate scums</h1>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+  
