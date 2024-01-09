749fde4b134e7edc6e0496d92f0e0921e1898824
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-01-09 14:20:51 +0100

    update date format on calender

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -45,7 +45,7 @@ const Scheduler = ({ Calender }) => {
         <ScheduleComponent
           height="650px"
           ref={(schedule) => setScheduleObj(schedule)}
-          selectedDate={new Date(2021, 0, 10)}
+          selectedDate={new Date()}
           eventSettings={{ dataSource: scheduleData }}
           dragStart={onDragStart}
         >
@@ -64,7 +64,7 @@ const Scheduler = ({ Calender }) => {
               <tr style={{ height: '50px' }}>
                 <td style={{ width: '100%' }}>
                   <DatePickerComponent
-                    value={new Date(2021, 0, 10)}
+                    value={new Date()}
                     showClearButton={false}
                     placeholder="Current Date"
                     floatLabelType="Always"
